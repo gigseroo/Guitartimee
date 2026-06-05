a3131346f443544b22fe47c40881d3824317625b
gigseroo <thegoatedraf@gmail.com>
2026-06-05 22:34:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function setTheme(theme) {
-    document.body.classList.remove("dark-theme","light-theme","cool-theme");
-    document.body.classList.add(theme + "-theme");
+    document.body.classList.remove("dark", "light", "cool");
+    document.body.classList.add(theme);
+
     localStorage.setItem("theme", theme);
 }
 
-window.onload = function () {
+window.addEventListener("DOMContentLoaded", () => {
     const saved = localStorage.getItem("theme") || "dark";
-    setTheme(saved);
+    document.body.classList.add(saved);
 
     const selector = document.getElementById("themeSelector");
     if (selector) selector.value = saved;
-};
+});
